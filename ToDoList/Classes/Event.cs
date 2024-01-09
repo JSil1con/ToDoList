670a4328b412753d8038c68b8d783624f2b9a0fd
JSil1con <daniel.kremen@email.cz>
2024-01-09 22:13:24 +0100

    Created event class

---
+++ ToDoList/Classes/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Classes
+{
+    internal class Event
+    {
+        public string Name { get; set; }
+        public string? Priority { get; set; }
+        public DateTime Date { get; set; }
+
+        public Event(string name, string? priority, DateTime dateTime)
+        {
+            Name = name;
+            Priority = priority;
+            Date = dateTime;
+        }
+
+        public Event(string name, DateTime dateTime)
+        {
+            Name = name;
+            Date = dateTime;
+        }
+    }
+}
