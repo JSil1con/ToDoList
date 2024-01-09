ed9e485b893a434a36d08cc8fb233f753260c306
JSil1con <daniel.kremen@email.cz>
2024-01-09 22:44:48 +0100

    Rename filehandler class

---
+++ ToDoList/Classes/FileHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ToDoList.Classes
+{
+    internal class FileHandler
+    {
+        private readonly string _path;
+        private readonly FileStream _fileStream;
+        private readonly StreamWriter _streamWriter;
+        private readonly StreamReader _streamReader;
+
+        public FileHandler(string name, string path)
+        {
+            _path = path;
+
+            _fileStream = File.Create(_path);
+
+            _streamWriter = new StreamWriter(_fileStream);
+            _streamReader = new StreamReader(_fileStream);
+        }
+
+        private bool FileExists()
+        {
+            if (File.Exists(_path)) return true;
+            return false;
+        }
+
+        public void InsertIntoFile(string content)
+        {
+            _streamWriter.Write(content);
+        }
+
+        public string Read()
+        {
+            return _streamReader.ReadToEnd();
+        }
+    }
+}
