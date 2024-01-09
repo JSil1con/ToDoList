693e38e85360b809406228ad86e9186d117c7c22
JSil1con <daniel.kremen@email.cz>
2024-01-09 23:16:17 +0100

    Created FileHandler in program

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -2,9 +2,8 @@
 using System.IO;
 using System.Reflection;
 
-string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "output.json");
 
-FileHandler fileHander = new FileHandler(path);
+FileHandler fileHander = new FileHandler("output.json");
 
 
 Console.ReadLine();
