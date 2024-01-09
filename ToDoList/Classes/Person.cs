7912868ff7f2ceba36c7057b64cba698243eb195
JSil1con <daniel.kremen@email.cz>
2024-01-09 22:18:27 +0100

    Created person class

---
+++ ToDoList/Classes/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Classes
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        private Dictionary<string, Event> _events;
+
+        public Person()
+        {
+
+        }
+    }
+}
